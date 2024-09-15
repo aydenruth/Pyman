bbfb5f0aaf3f9bf3a2b98c900f55195fd3bc1c8a
aydenruth <aydenr2@outlook.com>
2024-09-15 15:09:08 -0400

    Basic game functionality complete

--- src.py
+++ src.py
@@ -4,14 +4,20 @@ def hangman():
 
     goodWord = input("Set word:")
     print(goodWord)
-    score = len(goodWord)
+    score = 0
 
     for i in goodWord:
         guess = input("Enter a Letter:")
-        if guess == goodWord[i]:
+        print(i)
+        if guess == i:
             score += 1
         else:
             print("yeouch")
 
+
     if score == len(goodWord):
         print("You Win!")
+    else:
+        print("You Lose ;'(")
+
+hangman()
