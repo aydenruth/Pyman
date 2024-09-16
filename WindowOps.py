8c352f75db19ee792c24a3ec52d9dbf73c7d9ffd
aydenruth <aydenr2@outlook.com>
2024-09-16 14:54:50 -0400

    window layout complete

--- WindowOps.py
+++ WindowOps.py
@@ -5,8 +5,13 @@ window.title("Pyman")
 window.geometry('900x600')
 
 textbox = tk.Entry(master = window, text = "Enter your letter here!", width = 66, justify = "center")
+lengthBox = tk.Text(width = "100", height = "20", background = "purple")
+nuhUhBox = tk.Text(width = "100", height = "20", background = "blue")
 pymanBox = tk.Canvas(master = window, width = 400, height = 500, background="green")
+
 pymanBox.place(x = "0", y = "0")
+lengthBox.place(x = "400")
+nuhUhBox.place(x = "400", y = "250")
 textbox.place(x = "0", y = "500")
 window.mainloop()
 
