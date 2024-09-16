4fde2e02fb8734ae2f41ba5f7dd3f507eb5ec18b
aydenruth <aydenr2@outlook.com>
2024-09-16 12:35:55 -0400

    basic window layout started

--- WindowOps.py
+++ WindowOps.py
@@ -3,3 +3,11 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Pyman")
 window.geometry('900x600')
+
+textbox = tk.Entry(master = window, text = "Enter your letter here!", width = 66, justify = "center")
+pymanBox = tk.Canvas(master = window, width = 400, height = 500, background="green")
+pymanBox.place(x = "0", y = "0")
+textbox.place(x = "0", y = "500")
+window.mainloop()
+
+
--- src.py
+++ src.py
@@ -1,28 +1,3 @@
-import tkinter
-import random
-
-def hangman():
-
-    goodWord = input("Set word, or type ""1player"" to play vs computer")
-    if goodWord == "1player":
-      goodWord = []
-
-    
-    print(goodWord)
-    score = 0
-
-    for i in goodWord:
-        guess = input("Enter a Letter:")
-        print(i)
-        if guess == i:
-            score += 1
-        else:
-            print("yeouch")
-
-
-    if score == len(goodWord):
-        print("You Win!")
-    else:
-        print("You Lose ;'(")
+from hangmanGame import hangman
 
 hangman()
