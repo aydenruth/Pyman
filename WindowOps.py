ad5b09b890fd4dd988085ad9e8d1afa807299f93
aydenruth <aydenr2@outlook.com>
2024-09-17 13:12:50 -0400

    GUI shows word length

--- WindowOps.py
+++ WindowOps.py
@@ -2,18 +2,21 @@ import tkinter as tk
 import random
 
 randomChoices = ["Cat", "Bat", "Rat", "Thank", "Quark", "Interlinked", "Cells", "Within", "Gerard", "Meow", "Shoe", "Eels", "Persona", "Teeth", "Eyes"]
-score = 0
 goodlen = 0
 goodWord = ""
+numLetters = "THREE"
+letterBank = ["A", "B", "C", "D", "E", "F", "G"]
+
+score = 0
+random.seed()
+goodWord = randomChoices[random.randint(0, 14)] 
+print(goodWord)
+
+goodlen = len(goodWord)
+numLetters = "  _" * goodlen
+print(numLetters)
 
 def hangman():
-
-    goodWord = input("Set word, or type ""1player"" to play vs computer")
-    if goodWord == "1player":
-      random.seed()
-      goodWord = randomChoices[random.randint(0, 14)] 
-
-    goodlen = len(goodWord)
 
     for i in goodWord:
         guess = input("Enter a Letter:")
@@ -29,21 +32,23 @@ def hangman():
     else:
         print("You Lose ;'(")
 
+#def formatLetters():
+
+ #   for i in letterBank:
+        #maybe an initial function to put letters into 2/3 lines, then a second one to replace any used ones
+
+
 
 window = tk.Tk()
 window.title("Pyman")
 window.geometry('900x600')
 
 #find a way to get hangman func in here & in proper scope
-lentext = tk.StringVar()
-lentext.set( " _" * goodlen)
-print(lentext)
-
 #usableLetters = [the whole alphabet]
 
 textbox = tk.Entry(master = window, text = "Enter your letter here!", width = 66, justify = "center")
-lengthBox = tk.Label(width = "100", height = "20", background = "purple", textvariable = lentext)
-nuhUhBox = tk.Text(width = "100", height = "20", background = "blue")
+lengthBox = tk.Label( master = window, width = "50", height = "15", background = "purple", text = numLetters, justify = "center", font = ("Helvetica", "16"))
+nuhUhBox = tk.Label(width = "100", height = "20", background = "blue", text =  )
 pymanBox = tk.Canvas(master = window, width = 400, height = 500, background="green")
 
 pymanBox.place(x = "0", y = "0")
@@ -55,3 +60,5 @@ hangman()
 window.mainloop()
 
 
+#number of dashes cant show up since the window pops up before the user inputs the word. 
+#cut the user picking a word
